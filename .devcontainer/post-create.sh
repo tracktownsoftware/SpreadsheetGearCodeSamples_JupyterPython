3a4fa44b44dd3d246d3c24c4653a514341481698
Chris Hamilton <chris@tracktownsoftware.com>
2023-03-22 07:20:53 -0700

    added NET7 SDK install

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,1 +1,11 @@
 pip3 install --user -r requirements.txt
+
+## Before installing .NET, add the Microsoft package signing key 
+## to your list of trusted keys and add the package repository
+wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+## Install the .NET SDK
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-7.0
